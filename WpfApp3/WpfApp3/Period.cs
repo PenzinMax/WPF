d2410eadca3a14035b06ea9f1e0a1c90dc41211c
PenzinMax <99172843+PenzinMax@users.noreply.github.com>
2024-03-13 11:51:37 +0500

    Add files via upload

---
+++ WpfApp3/WpfApp3/Period.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    public class Period
+    {
+        public DateTime DateStart { get; set; }
+        public DateTime DateFinish { get; set; }
+
+        public int Type { get; set; }
+    }
+}
